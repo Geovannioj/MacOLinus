2bcbfdff43a13c2ebb76be25b888c15c36b6d392
Geovanni Oliveira <geovannirock@gmail.com>
2017-04-19 15:15:03 -0300

    addition of the project.pbx file

---
+++ MiniChallenge/MiniChallenge/Reminder.swift
@@ -0,0 +1,9 @@
+//
+//  Reminder.swift
+//  MiniChallenge
+//
+//  Created by Geovanni Oliveira de Jesus on 19/04/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
