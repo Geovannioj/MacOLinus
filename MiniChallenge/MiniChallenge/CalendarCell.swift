691ec7a425702e383e342a63dd75913dad74a0b2
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2017-04-20 17:40:32 -0300

    Updating calendar first code

---
+++ MiniChallenge/MiniChallenge/CalendarCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarCell.swift
+//  MiniChallenge
+//
+//  Created by Gabriel Rodrigues on 20/04/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+
+    @IBOutlet weak var dateLabel: UILabel!
+}
---
+++ MiniChallenge/MiniChallenge/CalendarViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CalendarViewController.swift
+//  MiniChallenge
+//
+//  Created by Gabriel Rodrigues on 20/04/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension ViewController: JTAppleCalendarViewDelegate, JTAppleCalendarViewDataSource {
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.locale = Calendar.current.locale
+        formatter.timeZone = Calendar.current.timeZone
+        
+        
+        let startDate = formatter.date(from: "2017 01 01")
+        let endDate = formatter.date(from: "2017 12 31")
+        
+        let parameters = ConfigurationParameters(startDate: startDate!, endDate: endDate!)
+        
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        cell.dateLabel.text = cellState.text
+        return cell
+    }
+
+}
