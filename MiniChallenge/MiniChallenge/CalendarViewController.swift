7fe6d305ed57ca12a6e6f73b0da98ae03f4e148d
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2017-04-25 14:39:10 -0300

    Progress on calendar configuration and cells reaction

--- MiniChallenge/MiniChallenge/CalendarViewController.swift
+++ MiniChallenge/MiniChallenge/CalendarViewController.swift
@@ -27,3 +27,53 @@ class CalendarViewController: UIViewController {
     }
 
 }
+
+extension ViewController: JTAppleCalendarViewDataSource {
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy MM dd"
+        formatter.locale = Calendar.current.locale
+        formatter.timeZone = Calendar.current.timeZone
+        
+        
+        let startDate = formatter.date(from: "2017 01 01")
+        let endDate = formatter.date(from: "2017 12 31")
+        
+        let parameters = ConfigurationParameters(startDate: startDate!, endDate: endDate!)
+        
+        return parameters
+    }
+}
+
+extension ViewController: JTAppleCalendarViewDelegate {
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "CalendarCell", for: indexPath) as! CalendarCell
+        
+        cell.dateLabel?.text = cellState.text
+        cell.selectedCell.layer.cornerRadius = 12
+        
+        if cell.isSelected {
+            cell.selectedCell.isHidden = false
+        } else {
+            cell.selectedCell.isHidden = true
+        }
+        
+        return cell
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        
+        guard let selectedCell = cell as? CalendarCell else { return }
+        selectedCell.selectedCell.isHidden = false
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
+        
+        guard let selectedCell = cell as? CalendarCell else { return }
+        selectedCell.selectedCell.isHidden = true
+    }
+}
