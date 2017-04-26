baff6c8f7047338aee684b3987d8f5dd6bdc7d48
Geovanni Oliveira <geovannirock@gmail.com>
2017-04-26 17:52:47 -0300

    Cleaning some mess on the code

--- MiniChallenge/MiniChallenge/ReminderAddViewController.swift
+++ MiniChallenge/MiniChallenge/ReminderAddViewController.swift
@@ -9,47 +9,6 @@
 import UIKit
 import UserNotifications
 
-extension Date {
-    /// Returns the amount of years from another date
-    func years(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.year], from: date, to: self).year ?? 0
-    }
-    /// Returns the amount of months from another date
-    func months(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.month], from: date, to: self).month ?? 0
-    }
-    /// Returns the amount of weeks from another date
-    func weeks(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.weekOfYear], from: date, to: self).weekOfYear ?? 0
-    }
-    /// Returns the amount of days from another date
-    func days(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.day], from: date, to: self).day ?? 0
-    }
-    /// Returns the amount of hours from another date
-    func hours(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.hour], from: date, to: self).hour ?? 0
-    }
-    /// Returns the amount of minutes from another date
-    func minutes(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.minute], from: date, to: self).minute ?? 0
-    }
-    /// Returns the amount of seconds from another date
-    func seconds(from date: Date) -> Int {
-        return Calendar.current.dateComponents([.second], from: date, to: self).second ?? 0
-    }
-    /// Returns the a custom time interval description from another date
-    func offset(from date: Date) -> String {
-        if years(from: date)   > 0 { return "\(years(from: date))y"   }
-        if months(from: date)  > 0 { return "\(months(from: date))M"  }
-        if weeks(from: date)   > 0 { return "\(weeks(from: date))w"   }
-        if days(from: date)    > 0 { return "\(days(from: date))d"    }
-        if hours(from: date)   > 0 { return "\(hours(from: date))h"   }
-        if minutes(from: date) > 0 { return "\(minutes(from: date))m" }
-        if seconds(from: date) > 0 { return "\(seconds(from: date))s" }
-        return ""
-    }
-}
 
 protocol AddReminderViewControllerDelegate: class {
     func addReminderControllerDidCancel(_ controller: ReminderAddViewController)
@@ -90,54 +49,6 @@ class ReminderAddViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func checkDate() {
-        // Disable the Save button if date has passed
-        if (Date() as NSDate).earlierDate(timePicker.date) == timePicker.date {
-            saveButton.isEnabled = false
-        }
-    }
-    
-    // UITextFieldDelegate
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Hide the keyboard.
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        navigationItem.title = textField.text
-    }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        // Disable the Save button while editing.
-        saveButton.isEnabled = false
-    }
-    
-    // UIDatePickerDelegate
-    @IBAction func timeChanged(_ sender: UIDatePicker) {
-        checkDate()
-    }
-
-    @IBAction func createNotification(_ sender: UIButton) {
-        
-        let content = UNMutableNotificationContent()
-        content.title = titleField.description
-        content.subtitle = titleField.description
-        content.body = description.description
-        content.categoryIdentifier = "Message"
-        
-        print("")
-        print("TIMER PICKER DATE:", timePicker.date)
-        print("TIME INTERVAL", TimeInterval(Date().seconds(from: timePicker.date)))
-        print("")
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: -1*TimeInterval(Date().seconds(from: timePicker.date)), repeats: false)
-        let request = UNNotificationRequest(identifier: "asd", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
-        
-    }
     @IBAction func cancelNavBar(){
         delegate?.addReminderControllerDidCancel(self)
         dismiss(animated: true, completion: nil)
@@ -159,15 +70,5 @@ class ReminderAddViewController: UIViewController {
             delegate?.addReminderViewController(self, didFinishAdding: reminder)
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
