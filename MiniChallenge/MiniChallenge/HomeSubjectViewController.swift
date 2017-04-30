1957bc3edd5b733e67fdef95e211d8b0056856dc
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-04-30 07:59:48 -0300

    Modifying view and controller to showSubject

--- MiniChallenge/MiniChallenge/HomeSubjectViewController.swift
+++ MiniChallenge/MiniChallenge/HomeSubjectViewController.swift
@@ -2,34 +2,87 @@
 //  HomeSubjectViewController.swift
 //  MiniChallenge
 //
-//  Created by Miguel Pimentel on 29/04/17.
+//  Created by Miguel Pimentel on 27/04/17.
 //  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
 //
 
 import UIKit
 
-class HomeSubjectViewController: UIViewController {
-
+class HomeSubjectViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let cards = ["Name", "dsds"]
+    
+    
+    
+    @IBOutlet weak var subjectsCards: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        setBackgroundColor()
+        
+        
+        
         // Do any additional setup after loading the view.
+        
+        if cards.count == 0 {
+            subjectsCards.isHidden = true
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    
+    internal func setBackgroundColor() {
+        
+        let backgroungColor = UIColor(red: 0.9804, green: 0.4588, blue: 0.4431, alpha: 1.0)
+        self.view.backgroundColor = backgroungColor
+        
+        
     }
-    */
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        let numberOfSubjects = cards.count
+        return numberOfSubjects
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! SubjectCardTableViewCell
+        
+        cell.myLabel.text = cards[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        cell.alpha = 0
+        let rotation = CATransform3DTranslate(CATransform3DIdentity, -250, 20, 0)
+        cell.layer.transform = rotation
+        UIView.animate(withDuration: 0.9){
+            cell.alpha = 1
+            cell.layer.transform = CATransform3DIdentity
+            
+        }
+        
+        
+        cell.backgroundColor = UIColor.brown
+    }
+    
+    
+    
 }
+
+
