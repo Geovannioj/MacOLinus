862ab33de2e19ca0914103c267d93452d84e2f5b
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-04-30 07:51:38 -0300

    Inserting NSCoding in models and updating necessary attributes

---
+++ MiniChallenge/MiniChallenge/SubjectCardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SubjectCardTableViewCell.swift
+//  MiniChallenge
+//
+//  Created by Miguel Pimentel on 29/04/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import UIKit
+
+class SubjectCardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
