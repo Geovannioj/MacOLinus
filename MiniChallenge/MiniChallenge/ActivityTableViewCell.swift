61a404eea679ec95e782a9cb219edafef666ebb3
Gabriel Rodrigues <gabrielrodriguesfaria97@gmail.com>
2017-05-03 15:12:28 -0300

    Saving activity nib file

--- MiniChallenge/MiniChallenge/ActivityTableViewCell.swift
+++ MiniChallenge/MiniChallenge/ActivityTableViewCell.swift
@@ -2,7 +2,7 @@
 //  ActivityTableViewCell.swift
 //  MiniChallenge
 //
-//  Created by Gabriel Rodrigues on 28/04/17.
+//  Created by Gabriel Rodrigues on 03/05/17.
 //  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
 //
 
@@ -10,11 +10,15 @@ import UIKit
 
 class ActivityTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var dayLabel: UILabel!
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var activityName: UILabel!
-    @IBOutlet weak var activitySubject: UILabel!
-    @IBOutlet weak var activityHour: UILabel!
-    @IBOutlet weak var subjectColor: UILabel!
-    @IBOutlet weak var clockIcon: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
 }
