48960e489a31c00a24b87cee0ceb9520f8af9294
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-05-08 20:14:21 -0300

    Refactoring SubjectViewController

---
+++ MiniChallenge/MiniChallenge/SingletonSubject.swift
@@ -0,0 +1,13 @@
+//
+//  SingletonSubject.swift
+//  MiniChallenge
+//
+//  Created by Miguel Pimentel on 08/05/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import UIKit
+
+class SingletonSubject: NSObject {
+
+}
