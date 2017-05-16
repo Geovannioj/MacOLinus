00f8775f23b898dc8e41271f73fe9dc704d354eb
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-05-16 11:38:18 -0300

    Minimal changes

--- MiniChallenge/MiniChallenge/SelectGoalDateViewController.swift
+++ MiniChallenge/MiniChallenge/SelectGoalDateViewController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class SelectDateViewController: UIViewController {
+class SelectGoalDateViewController: UIViewController {
 
+    
+    @IBOutlet weak var mondayCheckbox: UIButton!
+    
+    var Checked = UIImage(named: "checkboxSelected")
+    var Unchecked = UIImage(named: "checkbokEmpty")
+    
+    var isMondaySelected: Bool = false
+    var isTuesaySelected: Bool = false
+    var isWednesdaySelected: Bool = false
+    var isThursdaySelected: Bool = false
+    var isFridaySelected: Bool = false
+    var isSaturdarSelected: Bool = false
+    var isSundaySelected: Bool = false
+    
+    
+    // MARK: - Checkboxes
+    
+    @IBAction func mondaySelected(_ sender: Any) {
+    
+        if isMondaySelected {
+            isMondaySelected = false
+        } else {
+            isMondaySelected = true
+        }
+        
+        if isMondaySelected {
+            mondayCheckbox.setImage(Checked, for: UIControlState.normal)
+        } else {
+            mondayCheckbox.setImage(Unchecked, for: UIControlState.normal)
+        }
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+        
+        setConfig()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -21,6 +58,35 @@ class SelectDateViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    
+    func assignBlackStatusBar() {
+        
+        UIApplication.shared.statusBarStyle = .default
+        
+    }
+    
+    internal func assignBackground() {
+        
+        let background = UIImage(named: "PurplePatternWithBoy")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
+    }
+    
+    
+    func setConfig() {
+        
+        assignBlackStatusBar()
+        assignBackground()
+        
+    }
 
     /*
     // MARK: - Navigation
