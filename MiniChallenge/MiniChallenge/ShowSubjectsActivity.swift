0fd45c9910491e97b9073e22bee12da76c55f648
Geovanni Oliveira <geovannirock@gmail.com>
2017-05-18 17:40:01 -0300

    creation of the screen to show activities related to the subject

---
+++ MiniChallenge/MiniChallenge/ShowSubjectsActivity.swift
@@ -0,0 +1,9 @@
+//
+//  ShowSubjectsActivity.swift
+//  MiniChallenge
+//
+//  Created by Geovanni Oliveira de Jesus on 18/05/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
