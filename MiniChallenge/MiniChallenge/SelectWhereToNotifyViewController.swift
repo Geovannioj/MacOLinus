86ece014550eb9efc0ef5985c6fb05792ab5299d
Miguel Pimentel <miguelpimentel2013@gmail.com>
2017-05-19 10:06:02 -0300

    Adding possibility to select where to be notified from map and geolocalizated notification

--- MiniChallenge/MiniChallenge/SelectWhereToNotifyViewController.swift
+++ MiniChallenge/MiniChallenge/SelectWhereToNotifyViewController.swift
@@ -17,9 +17,16 @@ class SelectWhereToNotifyViewController: UIViewController, MKMapViewDelegate, CL
     @IBOutlet weak var map: MKMapView!
     
     var locationManager = CLLocationManager()
+    var coordinateToNotify: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {
+            didAllow, error in
+            
+        })
         
         setConfig()
 
@@ -31,11 +38,7 @@ class SelectWhereToNotifyViewController: UIViewController, MKMapViewDelegate, CL
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func nextScreenPressed(_ sender: Any) {
-        performSegue(withIdentifier: "SelectDate", sender: Any?.self)
-    }
-    
-    
+
     
     // MARK: - General Setup
     
@@ -69,6 +72,8 @@ class SelectWhereToNotifyViewController: UIViewController, MKMapViewDelegate, CL
     }
     
     
+     // MARK: - Map Setup
+    
     func setupMap() {
         
         locationManager.delegate = self
@@ -93,18 +98,72 @@ class SelectWhereToNotifyViewController: UIViewController, MKMapViewDelegate, CL
         let region = MKCoordinateRegion(center: location, span: span)
         
         self.map.setRegion(region, animated: true)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(SelectWhereToNotifyViewController.longpress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 1.5
+        
+        map.addGestureRecognizer(uilpgr)
+        
+    }
+    
+    func longpress(gestureRecognizer: UIGestureRecognizer) {
+        
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        let annotation = MKPointAnnotation()
+        
+        coordinateToNotify = coordinate
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New place"
+        annotation.subtitle = "Maybe I'll go here too..."
+        
+        map.addAnnotation(annotation)
+        
     }
 
+    // MARK: - Navigation
+    
+    @IBAction func nextScreenPressed(_ sender: Any) {
+        
+        geolocalizatedNotification()
+        
+        performSegue(withIdentifier: "SelectDate", sender: Any?.self)
+    }
+    
+    // MARK - Notification
+    
+    func geolocalizatedNotification() {
+        
+        let latitude = coordinateToNotify.latitude
+        let longitude = coordinateToNotify.longitude
+        
+        let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = CLCircularRegion(center: center, radius: 2000.0, identifier: "Geolocalizated Notification")
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        
+        let trigger = UNLocationNotificationTrigger(region: region, repeats: true)
+    
+        let content = UNMutableNotificationContent()
+        content.title = "Pengo"
+        content.body = "Pengo is notifying you"
+        content.sound = UNNotificationSound.default()
+        
+        let request = UNNotificationRequest(identifier: "textNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("Uh oh! We had an error: \(error)")
+            }
+        }
+        
+    }
 
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
